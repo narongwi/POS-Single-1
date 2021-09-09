65e32fef79aeabe468539989c01a34d995fad927
deftsoft development <deftsoft.contact@gmail.com>
2021-09-09 10:49:52 +0700

    Add frmMember
    Add frmMember

---
+++ BJCBCPOS_Solution/BJCBCPOS/OtherServices/frmMember.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BJCBCPOS.OtherServices
+{
+    public partial class frmMember : Form
+    {
+        public frmMember()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMember_Load(object sender, EventArgs e)
+        {
+           // ClearAllData();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
